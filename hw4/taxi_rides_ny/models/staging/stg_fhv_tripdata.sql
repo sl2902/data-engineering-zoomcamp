909006438b8d44e42ad9e30b3ae11d84cc86b950
sl2902 <laxmsun@hotmail.com>
2023-02-21 02:27:02 +0530

    Initial commit

---
+++ hw4/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,21 @@
+{{ 
+    config(materialized='table') 
+}}
+
+
+select
+  Affiliated_base_number,
+  cast(PUlocationID as integer) as pickup_locationid,
+  cast(DOlocationID as integer) as dropoff_locationid,
+  cast(pickup_datetime as timestamp) as pickup_datetime,
+  cast(dropOff_datetime as timestamp) as dropoff_datetime,
+  SR_Flag,
+  dispatching_base_num
+from 
+    {{ source('staging', 'fhv_trips') }}
+-- where Affiliated_base_number is not null
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+    limit 100
+{% endif %}
--- hw4/web_to_gcs.py
+++ hw4/web_to_gcs.py
@@ -7,6 +7,7 @@ import pyarrow.parquet as pq
 from google.cloud import storage
 import time
 from pathlib import Path
+import argparse
 
 """
 Pre-reqs: 
@@ -15,9 +16,10 @@ Pre-reqs:
 """
 
 # services = ['fhv','green','yellow']
+base_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/"
 url = r"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv"
 # switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "de-hw3")
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "de-hw4")
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -44,27 +46,84 @@ def web_to_gcs(year, service):
         out_path = Path(f"data/{service}/{file_name}.parquet")
 
         # download it using requests via a pandas df
-        dataset_url = f"{url}/{file_name}.csv.gz"
-        df = pd.read_csv(dataset_url)
-        # r = requests.get(request_url)
-        # pd.DataFrame(io.StringIO(r.text)).to_csv(f"../data/{file_name}")
-        # df = pd.DataFrame(io.StringIO(r.text))
-        # df.columns = df.columns.astype(str)
-        df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
-        df["dropOff_datetime"] = pd.to_datetime(df["dropOff_datetime"])
-        df["PUlocationID"] = df["PUlocationID"].astype("Int64")
-        df["DOlocationID"] = df["DOlocationID"].astype("Int64")
-        df["SR_Flag"] = df["SR_Flag"].astype("Int64")
-        # schema = pa.scalar([
-        #     pa.field("dispatching_base_num", pa.string()),
-        #     pa.field("pickup_datetime", pa.timestamp()),
-        #     pa.field("dropOff_datetime", pa.timestamp()),
-        #     pa.field("PUlocationID", pa.int64()),
-        #     pa.field("DOlocationID", pa.int64()),
-        #     pa.field("SR_Flag", pa.int64()),
-        #     pa.field("Affiliated_base_number", pa.string())
-        # ])
-                # pa_table = pa.Table.from_pandas(df, schema=schema)
+        if not Path.is_file(out_path):
+            url = f"{base_url}{service}"
+            dataset_url = f"{url}/{file_name}.csv.gz"
+            green_schema_dict = {
+                    'VendorID': 'Int64',
+                    'passenger_count': 'Int64',
+                    'trip_distance': 'float64',
+                    'RatecodeID': 'Int64',
+                    'store_and_fwd_flag': 'object',
+                    'PULocationID': 'Int64',
+                    'DOLocationID': 'Int64',
+                    'payment_type': 'Int64',
+                    'fare_amount': 'float64',
+                    'extra': 'float64',
+                    'mta_tax': 'float64',
+                    'tip_amount': 'float64',
+                    'tolls_amount': 'float64',
+                    'improvement_surcharge': 'float64',
+                    'total_amount': 'float64',
+                    'congestion_surcharge': 'float64',
+                    'trip_type': 'Int64',
+                    'lpep_pickup_datetime': 'string',
+                    'lpep_dropoff_datetime': 'string',
+                    'ehail_fee': 'float64'
+                    }
+            yellow_schema_dict = {
+                    'VendorID': 'Int64',
+                    'passenger_count': 'Int64',
+                    'trip_distance': 'float64',
+                    'RatecodeID': 'Int64',
+                    'store_and_fwd_flag': 'object',
+                    'PULocationID': 'Int64',
+                    'DOLocationID': 'Int64',
+                    'payment_type': 'Int64',
+                    'fare_amount': 'float64',
+                    'extra': 'float64',
+                    'mta_tax': 'float64',
+                    'tip_amount': 'float64',
+                    'tolls_amount': 'float64',
+                    'improvement_surcharge': 'float64',
+                    'total_amount': 'float64',
+                    'congestion_surcharge': 'float64',
+                    'trip_type': 'Int64',
+                    'tpep_pickup_datetime': 'string',
+                    'tpep_dropoff_datetime': 'string',
+                    'ehail_fee': 'float64'
+                    }
+            if service == "green":
+                df = pd.read_csv(dataset_url, low_memory=False, dtype=green_schema_dict)
+            elif service == "yellow":
+                df = pd.read_csv(dataset_url, low_memory=False, dtype=yellow_schema_dict)
+        else:
+            # performing this step as in Bigquery when
+            # creating external table, vendorid is stored
+            # as float and casting it to int throws error
+            # the error is not resolved even after specifying
+            # a schema.
+            # Note - this fixed the issue with vendorid, but
+            # caused an issue with the macro case statement.
+            # I have to specify the dtype for each field as shown
+            # above in the if-statement
+            dataset_url = out_path
+            df = pd.read_parquet(dataset_url)
+            for col in df.columns:
+                df[col] = df[col].astype(str)
+        # if service == "fhv":
+        #     df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+        #     df["dropOff_datetime"] = pd.to_datetime(df["dropOff_datetime"])
+        #     df["SR_Flag"] = df["SR_Flag"].astype("Int64")
+        #     df["PUlocationID"] = df["PUlocationID"].astype("Int64")
+        #     df["DOlocationID"] = df["DOlocationID"].astype("Int64")
+        # elif service == "yellow":
+        #     df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        #     df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+        # elif service == "green":
+        #     df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+        #     df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+        # pa_table = pa.Table.from_pandas(df, schema=schema)
         # pq.write_table(pa_table, out_path)
         df.to_parquet(out_path, engine="pyarrow")
 
@@ -83,7 +142,26 @@ def web_to_gcs(year, service):
         print(f"GCS: {service}/{out_path}")
 
 if __name__ == "__main__":
+    year = 2020
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    color = "green"
     start = time.time()
-    web_to_gcs('2019', 'fhv')
+    parser = argparse.ArgumentParser("ETL Web to GCS")
+    parser.add_argument("--color", type=str, help="Input color of taxi. Choices are yellow/green", default="green")
+    parser.add_argument("--year", type=int, help="Add year of data upload", default=2021)
+    parser.add_argument("--months", type=int, nargs="+", help="List of months to process", default=[1])
+    # parser.add_argument("--url", type=str, help="NYC saxi Source url")
+    args = parser.parse_args()
+
+    if args.year:
+        year = args.year 
+    if args.color:
+        color = args.color
+    if args.months:
+        months = list(args.months)
+    # if args.url:
+    #     url = args.url
+
+    web_to_gcs(year, color)
     end = time.time()
     print(f"Web to GCS file transmission completed {(end-start)/60} mins")
