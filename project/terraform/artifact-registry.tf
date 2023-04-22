295e2dc73c9ae1da37cb6070ffe031b036035af3
sl2902 <laxmsun@hotmail.com>
2023-04-22 20:49:00 +0530

    Fixed mix up with artifact registry region name

--- project/terraform/artifact-registry.tf
+++ project/terraform/artifact-registry.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "my-project" {
   repository_id = "data-engineering-zoomcamp"
-  location      = var.COMPUTE_ENGINE_REGION
+  location      = var.GCP_REGION
   format        = "docker"
 }
