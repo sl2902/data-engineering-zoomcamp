70a88fa1ca165299ec427a3af455465ef0d7adea
sl2902 <laxmsun@hotmail.com>
2023-04-22 19:48:23 +0530

    Modified default date dataset

--- project/scripts/gcs_to_bq.py
+++ project/scripts/gcs_to_bq.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
     PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 
     parser = argparse.ArgumentParser("Pipeline to read from GCS and write to BigQuery")
-    parser.add_argument("--date", type=str, help="Enter period of load. Format yyyy-mm-dd", default="2022-04-25")
+    parser.add_argument("--date", type=str, help="Enter period of load. Format yyyy-mm-dd", default="2022-03-25")
 
     args = parser.parse_args()
     if args.date:
